f8799c649fd5aa01ff954e2791e539d5cdb24b19
GilarYa <gilarwahibul@gmail.com>
2023-10-06 11:10:43 +0700

    test

---
+++ config/lar.js
@@ -0,0 +1,83 @@
+import { get } from "https://jscroot.github.io/api/croot.js";
+import {responseData} from "../controller/controller.js"
+import {URLGeoJson} from "../controller/template.js"
+import {map} from './peta.js';
+import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover} from '../controller/popup.js';
+import {onClick} from 'https://jscroot.github.io/element/croot.js';
+import {getAllCoordinates} from '../controller/cog.js';
+// import
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("gisTable").getElementsByTagName('tbody')[0];
+
+    fetch("https://raw.githubusercontent.com/GilarYa/GISLAR/main/polyline.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Point") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.name;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("gisTable2").getElementsByTagName('tbody')[0];
+
+    fetch("https://raw.githubusercontent.com/GilarYa/GISLAR/main/polygon.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Polygon") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.nameb;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("gisTable3").getElementsByTagName('tbody')[0];
+
+    fetch("https://raw.githubusercontent.com/GilarYa/GISLAR/main/waypoint.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "LineString") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.name ;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+//panggil
+onClick('popup-closer',onClosePopupClick);
+onClick('insertmarkerbutton',onSubmitMarkerClick);
+onClick('hapusbutton',onDeleteMarkerClick);
+onClick('hitungcogbutton',getAllCoordinates);
+
+map.on('click', onMapClick);
+map.on('pointermove', onMapPointerMove);
+map.on('movestart', disposePopover);
+get(URLGeoJson,responseData);
